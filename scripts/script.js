d30ff1c9aba060352a298dcd2328caeb5e1fb7b3
evoworld1 <evoworld1@gmail.com>
2020-12-07 14:04:31 +0100

    Icons now are animated and game ends after player or computer score 5 points. Game now can be restarted with button. Work in progress.

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,15 @@ const game = () => {
   const gameplay = () => {
     const options = document.querySelectorAll(".options button");
     const computerOptions = ["rock", "paper", "scissors"];
+    const playerHand = document.querySelector(".player-hand img");
+    const computerHand = document.querySelector(".computer-hand img");
+    const bothHands = document.querySelectorAll(".hands img");
 
+    bothHands.forEach((hand) => {
+      hand.addEventListener("animationend", function () {
+        this.style.animation = "";
+      });
+    });
     options.forEach((option) => {
       option.addEventListener("click", function () {
         //Generating random number from 0-2
@@ -27,7 +35,18 @@ const game = () => {
         );
         const computerChoice = computerOptions[computerNumber]; //Applying random number to options array
         console.log(computerChoice);
-        scoreCompare(this.textContent, computerChoice);
+        setTimeout(() => {
+          scoreCompare(this.textContent, computerChoice);
+
+          //Images change
+
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 1300);
+
+        //Animation
+        playerHand.style.animation = "playerShake 1.5s ease";
+        computerHand.style.animation = "computerShake 1.5s ease";
       });
     });
   };
@@ -38,20 +57,6 @@ const game = () => {
     const computerScore = document.querySelector(".computer-score p");
     playerScore.textContent = pScore;
     computerScore.textContent = cScore;
-  };
-  //Ending the game and disabling buttons
-  const endgame = () => {
-    const disableBtn = document.querySelector(".options");
-
-    if (pScore === 5) {
-      pointer.textContent =
-        "Congratz! You win the game! Refresh to play again :)";
-      disableBtn.classList.add("disableButtons");
-    } else if (cScore === 5) {
-      pointer.textContent =
-        "Computer wins the game :( Refresh the page and play again! :)";
-      disableBtn.classList.add("disableButtons");
-    }
   };
 
   //Player's choice is compared with computer choice
@@ -81,7 +86,35 @@ const game = () => {
     }
   };
 
-  //executing inner functions
+  //Ending the game and disabling buttons
+  const endgame = () => {
+    const disableBtn = document.querySelector(".options");
+    const playAgain = document.querySelector(".play-again button");
+
+    function playAgainButton() {
+      playAgain.addEventListener("click", function () {
+        window.location.reload();
+      });
+    }
+
+    const restartGame = () => {
+      playAgain.style.visibility = "visible";
+      playAgain.style.animation = "popInOut 1s infinite running";
+    };
+
+    if (pScore === 5) {
+      pointer.textContent = "Congratz! You win the game!";
+      disableBtn.classList.add("disableButtons");
+      restartGame();
+      playAgainButton();
+    } else if (cScore === 5) {
+      pointer.textContent = "Computer wins the game :(";
+      disableBtn.classList.add("disableButtons");
+      restartGame();
+      playAgainButton();
+    }
+  };
+
   gameStart();
   gameplay();
 };
